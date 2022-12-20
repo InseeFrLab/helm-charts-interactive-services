8f7e9cdae8b90a35421f6901305d8082b9437238
avouacr <avouacr@pm.me>
2022-12-20 16:13:23 +0000

    minor changes

--- utils/generate-children-charts.py
+++ utils/generate-children-charts.py
@@ -1,11 +1,14 @@
 """Generate children charts from parent template."""
+from pathlib import Path
 import json
 import shutil
 
 import yaml
 
 
-with open("charts/children.yaml", "r") as file_in:
+PROJECT_PATH = Path(__file__).resolve().parents[1]
+
+with open(PROJECT_PATH / "charts" / "children.yaml", "r") as file_in:
     parameters = yaml.safe_load(file_in)
 
 for parent in parameters:
@@ -13,24 +16,24 @@ for parent in parameters:
         description_child = parameters[parent][child]["description"]
         images_child = parameters[parent][child]["images"]
 
-        parent_dir=f"charts/{parent}"
-        child_dir=f"charts/{child}"
+        parent_dir = PROJECT_PATH / "charts" / parent
+        child_dir = PROJECT_PATH / "charts" / child
 
         # Create child chart as a copy of parent chart
         shutil.copytree(src=parent_dir, dst=child_dir, dirs_exist_ok=True)
 
         # Correct Chart metadata
-        with open(f"{child_dir}/Chart.yaml", "r") as file_in:
+        with open(child_dir / "Chart.yaml", "r") as file_in:
             child_chart = yaml.safe_load(file_in)
 
         child_chart["name"] = child
         child_chart["description"] = description_child
 
-        with open(f"{child_dir}/Chart.yaml", "w") as file_out:
+        with open(child_dir / "Chart.yaml", "w") as file_out:
             yaml.dump(child_chart, file_out, sort_keys=False)
 
         # Fill child schema with proper values
-        with open(f"{child_dir}/values.schema.json", "r") as file_in:
+        with open(child_dir / "values.schema.json", "r") as file_in:
             child_schema = json.load(file_in)
 
         child_schema["properties"]["service"]["properties"]["image"]["properties"]["version"]["enum"] = images_child
@@ -51,5 +54,5 @@ for parent in parameters:
                 }
             child_schema["properties"]["resources"]["properties"]["limits"]["properties"]["nvidia.com/gpu"] = gpu_limits
 
-        with open(f"{child_dir}/values.schema.json", "w") as file_out:
+        with open(child_dir / "values.schema.json", "w") as file_out:
             json.dump(child_schema, file_out, indent=2, sort_keys=False)
