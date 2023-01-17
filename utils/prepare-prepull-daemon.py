11ea52a310f9da9f1091185eb133be85bf7a14bd
avouacr <avouacr@pm.me>
2023-01-17 17:59:21 +0000

    use Kubernetes Python API to automatize images pull

--- utils/prepare-prepull-daemon.py
+++ utils/prepare-prepull-daemon.py
@@ -4,9 +4,17 @@ import os
 import json
 
 import yaml
+import kubernetes
 
 
 PROJECT_PATH = Path(__file__).resolve().parents[1]
+
+# Configure Kubernetes Python API
+NAMESPACE = "user-avouacr"
+kube_config = kubernetes.config.load_incluster_config()
+with kubernetes.client.ApiClient(kube_config) as api_client:
+    kube_apps_api = kubernetes.client.AppsV1Api(api_client)
+kube_core_api = kubernetes.client.CoreV1Api()
 
 # Extract list of images to pre-pull from charts
 images_to_prepull = []
@@ -18,11 +26,11 @@ for chart in charts:
     images = chart_schema["properties"]["service"]["properties"]["image"]["properties"]["version"]["enum"]
     images_to_prepull.extend(images)
 
-# Fill DaemonSet template with one init container per image to pre-pull
-with open(PROJECT_PATH / "utils" / "prepull-daemon-template.yaml", "r") as file_in:
-    daemon_template = yaml.safe_load(file_in)
+# Fill template with one init container per image to pre-pull
+with open(PROJECT_PATH / "utils" / "prepull-template.yaml", "r") as file_in:
+    manifest = yaml.safe_load(file_in)
 
-for image in images_to_prepull:
+for image in images_to_prepull[:2]:
     init_container = {
         "name": image.split("/")[1].replace(":", "-").replace(".", "-"),
         "image": image,
@@ -30,7 +38,33 @@ for image in images_to_prepull:
         "imagePullPolicy": "Always",
         "resources": {"limits": {"cpu": "100m", "memory": "100Mi"}}
     }
-    daemon_template["spec"]["template"]["spec"]["initContainers"].append(init_container)
+    manifest["spec"]["template"]["spec"]["initContainers"].append(init_container)
 
-with open(PROJECT_PATH / "utils" / "prepull-daemon-manifest.yaml", "w") as file_out:
-    yaml.dump(daemon_template, file_out, sort_keys=False)
+# 1st step : create a Deployment to pull the images in the global registry cache once
+manifest["kind"] = "Deployment"
+kube_apps_api.create_namespaced_deployment(namespace=NAMESPACE, body=manifest)
+
+# Wait for the Deployment to be in Running state and remove it
+w = kubernetes.watch.Watch()
+for event in w.stream(kube_core_api.list_namespaced_pod, namespace=NAMESPACE,
+                      label_selector="name=prepull"):
+    pod_state = event['object'].status.phase
+    if pod_state == "Running":
+        w.stop()
+        kube_apps_api.delete_namespaced_deployment(namespace=NAMESPACE, name="prepull")
+        break
+
+# 2nd step : create a DaemonSet to pull the images from cache on all workers
+manifest["kind"] = "DaemonSet"
+kube_apps_api.create_namespaced_daemon_set(namespace=NAMESPACE, body=manifest)
+res = kube_apps_api.list_namespaced_daemon_set(namespace=NAMESPACE, label_selector="name=prepull")
+
+# Wait for all daemons to be in Running state and remove the DaemonSet
+w = kubernetes.watch.Watch()
+for event in w.stream(kube_apps_api.list_namespaced_daemon_set, namespace=NAMESPACE,
+                      label_selector="name=prepull"):
+    n_daemons_ready = event['object'].status.number_ready
+    if n_daemons_ready == 11:
+        w.stop()
+        kube_apps_api.delete_namespaced_daemon_set(namespace=NAMESPACE, name="prepull")
+        break
