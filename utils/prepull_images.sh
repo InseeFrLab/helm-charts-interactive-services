409479ac4f797b950e3ea406e7dc26e5459d8e2b
avouacr <avouacr@pm.me>
2023-03-01 16:37:09 +0000

    export kube namespace env

--- utils/prepull_images.sh
+++ utils/prepull_images.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-KUBERNETES_NAMESPACE=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
+export KUBERNETES_NAMESPACE=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
+
+env | grep -i KUBERNETES_NAMESPACE
 
 pip install -y kubernetes
 python prepull_images.py $KUBERNETES_NAMESPACE
