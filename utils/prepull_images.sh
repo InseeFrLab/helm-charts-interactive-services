721b12cd217aca2211c36aa746b2e8a689690c57
Romain Avouac <43444134+avouacr@users.noreply.github.com>
2023-07-10 11:20:56 +0200

    Update prepull_images.sh

--- utils/prepull_images.sh
+++ utils/prepull_images.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-kubectl delete deployments.apps --ignore-not-found=true prepull
-kubectl delete daemonsets.apps --ignore-not-found=true prepull
-
-pip install kubernetes
-
 KUBERNETES_NAMESPACE=`cat /var/run/secrets/kubernetes.io/serviceaccount/namespace`
 
 # IMAGES_TO_PREPULL='inseefrlab/onyxia-vscode-python:py3.10.9'
 python prepull_images.py $KUBERNETES_NAMESPACE $IMAGES_TO_PREPULL
+
+kubectl delete deployments.apps --ignore-not-found=true prepull
+kubectl delete daemonsets.apps --ignore-not-found=true prepull
